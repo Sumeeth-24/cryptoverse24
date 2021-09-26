b66cbb65bc9d5f560a4151d102f38ae03869145c
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2021-09-26 14:43:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, HomePage, Cryptocurrencies, Cryptodetails, News } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+         <Navbar />
+      </div>
+      <div className="main">
+          <Layout>
+            <div className="routes">
+             <Switch>
+               <Route exact path="/">
+                 <HomePage />
+               </Route>
+               <Route exact path="/exchanges">
+                 <Exchanges />
+               </Route>
+               <Route exact path="/cryptocurrencies">
+                 <Cryptocurrencies />
+               </Route>
+               <Route exact path="/crypto/:coinId">
+                 <Cryptodetails />
+               </Route>
+               <Route exact path="/news">
+                 <News />
+               </Route>
+             </Switch>
+            </div>
+          </Layout>
+     
+            <div className="footer" >
+                <Typography.Title level={5} style={{ color: 'white', textAlign:'center'}}>
+                  Cryptoverse <br />
+                  All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+
+            </div> 
+    </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+
+import store from './app/store';
+
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+       <App />
+    </Provider>
+  </Router>,  
+  document.getElementById('root')
+);
+
+
